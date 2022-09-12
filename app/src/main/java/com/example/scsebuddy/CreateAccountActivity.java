566242b5f0dbbbe873b8897009465e7157145758
Kristiyan Cholakov <krischolakov@icloud.com>
2022-09-12 13:18:52 +0800

    renaming files

--- app/src/main/java/com/example/scsebuddy/CreateAccountActivity.java
+++ app/src/main/java/com/example/scsebuddy/CreateAccountActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class create_account extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
