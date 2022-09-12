8cae549a65ac43131d678c97ba97074d73edec19
theodoretwl <theodoretwl@gmail.com>
2022-09-12 21:31:28 +0800

    Updated forum and courses to have basic buttons

--- app/src/main/java/com/example/scsebuddy/CourseActivity.java
+++ app/src/main/java/com/example/scsebuddy/CourseActivity.java
@@ -2,7 +2,9 @@ package com.example.scsebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CourseActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+    }
+
+    //Bottom buttons
+    public void mapScreen (View v){
+        Intent intent = new Intent(v.getContext(),MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void courseScreen(View v){
+        Intent intent = new Intent(v.getContext(),CourseActivity.class);
+        startActivity(intent);
+    }
+
+    public void forumScreen (View v){
+        Intent intent = new Intent(v.getContext(),ForumActivity.class);
+        startActivity(intent);
+    }
+
+    public void profileScreen (View v){
+        Intent intent = new Intent(v.getContext(),ProfileActivity.class);
+        startActivity(intent);
     }
 }
