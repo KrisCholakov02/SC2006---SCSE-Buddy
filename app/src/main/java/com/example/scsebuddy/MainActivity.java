9e9458dbe5673a9ca231aa0dbcf176d5084abb3b
theodoretwl <theodoretwl@gmail.com>
2022-09-12 11:04:36 +0800

    Edit map activity

--- app/src/main/java/com/example/scsebuddy/MainActivity.java
+++ app/src/main/java/com/example/scsebuddy/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//8F8E8E
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
