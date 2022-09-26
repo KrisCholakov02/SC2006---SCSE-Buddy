5c215d81e47819e162f16fa592cb7fd91996d7e8
Kristiyan Cholakov <krischolakov@icloud.com>
2022-09-26 18:40:32 +0800

    server

--- app/src/main/java/com/example/scsebuddy/LoginActivity.java
+++ app/src/main/java/com/example/scsebuddy/LoginActivity.java
@@ -7,10 +7,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.os.Bundle;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.scsebuddy.requestsresults.LoginResult;
+import com.example.scsebuddy.requestsresults.RetrofitInterface;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://192.168.0.102:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,41 @@ public class LoginActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+        this.findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, String> map = new HashMap<>();
+                EditText emailInput = findViewById(R.id.editTextEmail);
+                EditText passwordInput = findViewById(R.id.editTextPassword);
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                map.put("email", email);
+                map.put("password", password);
+
+                Call<LoginResult> call = retrofitInterface.executeLogin(map);
+
+                call.enqueue(new Callback<LoginResult>() {
+                    @Override
+                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
+                        if (response.code() == 200) {
+                            Intent intent = new Intent(v.getContext(), MapActivity.class);
+                            startActivity(intent);
+                        } else if (response.code() == 404) {
+                            Toast.makeText(LoginActivity.this, "Wrong Credential", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResult> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 
     public void skipLogin (View v) {
@@ -45,5 +96,10 @@ public class LoginActivity extends AppCompatActivity {
 
         AlertDialog alert11 = builder1.create();
         alert11.show();
+    }
+
+    public void handleLogin() {
+        View view = getLayoutInflater().inflate(R.layout.activity_login, null);
+
     }
 }
