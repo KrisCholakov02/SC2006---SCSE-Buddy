24bf21f07805b1efd0881caee470e2e7a3331fc5
theodoretwl <theodoretwl@gmail.com>
2022-09-28 01:46:30 +0800

    Added more activitys for forum, course and topic

---
+++ app/src/main/java/com/example/scsebuddy/TopicSuggestActivity.java
@@ -0,0 +1,14 @@
+package com.example.scsebuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopicSuggestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_suggest);
+    }
+}
