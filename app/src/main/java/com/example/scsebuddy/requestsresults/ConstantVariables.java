ad9a3b9580b59b6fe8eb32ef1f57635c45f5106f
Kristiyan Cholakov <krischolakov@icloud.com>
2022-10-03 09:41:59 +0800

    change server ip

--- app/src/main/java/com/example/scsebuddy/requestsresults/ConstantVariables.java
+++ app/src/main/java/com/example/scsebuddy/requestsresults/ConstantVariables.java
@@ -1,7 +1,7 @@
 package com.example.scsebuddy.requestsresults;
 
 public class ConstantVariables {
-    private static final String SERVER_URL = "http://scsebuddy.ddns.net:3000";
+    private static final String SERVER_URL = "http://10.27.22.146:3000";
 
     public static String getSERVER_URL() {
         return SERVER_URL;
