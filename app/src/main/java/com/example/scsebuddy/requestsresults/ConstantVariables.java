0eaa49807e8d66b8600d10a8fc0eee2ddb271715
theodoretwl <theodoretwl@gmail.com>
2022-10-22 00:02:28 +0800

    Added all images to drawables

--- app/src/main/java/com/example/scsebuddy/requestsresults/ConstantVariables.java
+++ app/src/main/java/com/example/scsebuddy/requestsresults/ConstantVariables.java
@@ -1,7 +1,7 @@
 package com.example.scsebuddy.requestsresults;
 
 public class ConstantVariables {
-    private static final String SERVER_URL = "http://42.61.234.190:3000";
+    private static final String SERVER_URL = "http://scsebuddy.ddns.net:3000";
 
     public static String getSERVER_URL() {
         return SERVER_URL;
