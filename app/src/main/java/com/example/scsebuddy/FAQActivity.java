b8c304687cdcb5c1a302f5245e058936aaf86626
theodoretwl <theodoretwl@gmail.com>
2022-11-06 00:08:54 +0800

    Hopefully final commit, updated FAQ

--- app/src/main/java/com/example/scsebuddy/FAQActivity.java
+++ app/src/main/java/com/example/scsebuddy/FAQActivity.java
@@ -4,15 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FAQActivity extends AppCompatActivity {
-
+    private int count = 0;
+    TextView faqTextView;
+    ImageView faqImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        faqTextView = this.findViewById(R.id.faqTextView);
+        faqImageView = this.findViewById(R.id.faqImageView);
+
+        faqTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.e("TEST", "hello");
+                count++;
+                if(count >15){
+                    faqImageView.setVisibility(View.VISIBLE);
+
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            // this code will be executed after 2 seconds
+                            count=0;
+                            faqImageView.setVisibility(View.INVISIBLE);
+                        }
+                    }, 2000);
+                }
+            }
+        });
     }
+
 
     //Bottom buttons
     public void mapScreen (View v){
